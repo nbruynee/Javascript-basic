d5fb932e4954553ef75b25714d188fdf195b6827
BruyneHuu <buinghialk180303@gmail.com>
2024-12-14 16:09:03 +0700

    update code video 7

---
+++ video7/video7.js
@@ -0,0 +1,11 @@
+console.log("Hello World")
+let a = 5;
+let b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+// \n or \ mean break page
+// 2 print it same but
+// recommend should use below 
+console.log(`String value of a is: ${a} and 
+string value of b is: ${b}`)
+console.log(`Fifteen is: ${a + b} 
+and not: ${2* a + b}.`)
