770f9d5806bbb133cf5091fe7221107a71fa6794
BruyneHuu <buinghialk180303@gmail.com>
2024-12-14 15:34:38 +0700

    update video5

--- video4/video4.js
+++
@@ -1,9 +0,0 @@
-let a = `I'm string - chain`;
-let b = 4
-let text = `We are the so-called 
-"Vikings" from the north`;
-
-console.log(text)
-console.log(a.trim().length) // trim() leave (bỏ lại) space behind
-console.log(b + 5 + "a")
-console.log("a" + 5 + b)
