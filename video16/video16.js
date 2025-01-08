ff00b294bffc5b8f6158da4c1d50d735a1acf4be
BruyneHuu <buinghialk180303@gmail.com>
2025-01-08 12:20:52 +0700

    update video16

---
+++ video16/video16.js
@@ -0,0 +1,18 @@
+console.log(`Hello world from html`)
+
+// input parameter (tham số)
+function sum(a, b) {
+    return a + b; 
+}
+
+let c = sum(2,3)
+
+// console.log(`Sum of a + b = ${sum(1,2)}`)
+console.log(`Sum of a + b = ${c}`)
+
+
+// note:
+// Why use functions, write less code but can reuse code
+// It's more secure to write the function first, then assign the value later.
+// set (đặt). Ex: set name is: Bruyne  
+// get (lấy về). Ex: get name will get name Bruyne that was set before.
