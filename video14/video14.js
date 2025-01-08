91a52731bc9dd5a3a01e215351337732a16d66ff
BruyneHuu <buinghialk180303@gmail.com>
2025-01-08 10:59:52 +0700

    update video14

---
+++ video14/video14.js
@@ -0,0 +1,49 @@
+console.log(`Hello World from html`)
+
+let day = '';
+// switch (new Date().getDay()) {
+//     case 0:
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+let a = 4; 
+switch (a) {
+    case 0:
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+console.log(`Check Date: ${day}`)
