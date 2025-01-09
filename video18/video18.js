874255ef3ed807627eab55b7f5ac0fed4bec17c5
BruyneHuu <buinghialk180303@gmail.com>
2025-01-09 17:35:08 +0700

    update video18

---
+++ video18/video18.js
@@ -0,0 +1,14 @@
+console.log(`Hello World from html`)
+
+// review previous lesson
+// function sum(a,b) {
+//     return a + b; 
+// }
+
+// arrow function
+// this function will more use function above
+let sum = (a,b) => {
+    return a + b; 
+}
+
+console.log(`Sum is: ${sum(6,9)}`)
