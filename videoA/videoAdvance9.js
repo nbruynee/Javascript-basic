3f66a97bbb27175b06f868c7470d0dab9e94834c
BruyneHuu <buinghialk180303@gmail.com>
2025-01-31 16:09:08 +0700

    update video advance 8

--- videoA/videoAdvance9.js
+++ videoA/videoAdvance9.js
@@ -1,9 +1,5 @@
 console.log(`Hello World from HTML`)
-// + chain Promises
-
-// When you need to perform multiple asynchronous operations in sequence(bộ tuần tự) (e.g. consecutive(liên tiếp) API calls),
-// instead of nesting callbacks (callback hell), you can chain promises by calling .then() one after another.   
-// Promise chaining: Each .then() returns a new Promise. The result of one .then() is passed to the next .then()
+// Fetch API 
 
 const callback = (err, data) => {
     if (err) {
@@ -34,26 +30,50 @@ function getTodos(id, callback) {
         requests.send();
     })
 }
+// -----------------------------------------------------------------------------------------------------------
+// fetch('https://jsonplaceholder.typicode.com/todos/1').then(data => {
+//     console.log(`Check Fetch Data: `, data);
+// })
 
-getTodos(1).then(data1 => {
-    console.log(`>>>Data 1:`, data1);
-    return getTodos(2);
+// fetch() returns a Promise containing the Response object.
+// When you call console.log(data), you are only printing out (in ra) the Response object's 
+// information, not getting(không chứa) the JSON content from the response.
+// Problem: You don't get the actual(thực) data (body) from the response because you haven't called .json().
+
+// -----------------------------------------------------------------------------------------------------------
+// fetch('https://jsonplaceholder.typicode.com/todos/1').then(data => {
+//     let data1 = data.json();
+//     console.log(`Check Fetch Data: `, data1);
+// })
+
+// data.json() returns a Promise, not the live data.
+// It's printing out the Promise {<pending>}, not the JSON content.
+// Problem: Need to wait (await) or handle with .then() to get result from data.json().
+
+// -----------------------------------------------------------------------------------------------------------
+fetch('https://jsonplaceholder.typicode.com/todos/1').then(data => {
+    return data.json()
+}).then(data => {
+    console.log(`Check Fetch Data: `, data);
 })
-    .then(data2 => {
-        console.log(`>>>Data 2:`, data2);
-        return getTodos(3)
-    })
-    .then(data3 => {
-        console.log(`>>>Data 3:`, data3);
-    })
-    .catch(error => {
-        console.log(`Something wrong:`, error)
-    })
+// data.json() returns a Promise containing the JSON parse result.
+// Use .then() next to handle the result when the Promise is resolved.
 
-// Call getTodos(1) and wait for the result.
-// If successful, call getTodos(2).
-// If successful, call getTodos(3).
-// If there is an error at any step, .catch() will handle the error.
+// getTodos(1).then(data1 => {
+//     console.log(`>>>Data 1:`, data1);
+//     return getTodos(2);
+// })
+//     .then(data2 => {
+//         console.log(`>>>Data 2:`, data2);
+//         return getTodos(3)
+//     })
+//     .then(data3 => {
+//         console.log(`>>>Data 3:`, data3);
+//     })
+//     .catch(error => {
+//         console.log(`Something wrong:`, error)
+//     })
+
 
 
 
